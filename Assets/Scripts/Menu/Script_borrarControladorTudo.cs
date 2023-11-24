f492d034eeb5a610ccc0eccbd91445b91c209020
Boneco Sinforoso <icmgc98@gmail.com>
2023-11-24 20:39:05 -0300

    borrado 50%

---
+++ Assets/Scripts/Menu/Script_borrarControladorTudo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_borrarControladorTudo : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        Debug.Log("kasdasd");
+    }
+}
