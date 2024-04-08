902c906ae64cceffc62c06690f4b2234e9cbf423
Boneco Sinforoso <icmgc98@gmail.com>
2024-04-08 19:37:14 -0300

    linhas de deteccao

--- Assets/_ME/Scripts/Infinity/Script_iaInfinityTudo.cs
+++ Assets/_ME/Scripts/Infinity/Script_iaInfinityTudo.cs
@@ -22,6 +22,8 @@ public class Script_iaInfinityTudo : MonoBehaviour
     [SerializeField] bool bloq_cen;
     [SerializeField] bool bloq_dir;
 
+    [SerializeField] LineRenderer[] lr;
+
     bool sentidoMudarPode = true;
 
     void Start()
@@ -60,6 +62,41 @@ public class Script_iaInfinityTudo : MonoBehaviour
         if (Physics.Raycast(new Vector3(22.5f, 0.5f, transform.position.z), Vector3.forward, 6f, 1 << 3)) bloq_dir = true;
         if (Physics.Raycast(new Vector3(22.5f, 2f, transform.position.z), Vector3.down, 2f, 1 << 3)) bloq_dir = true;
 
+        lr[0].startColor = bloq_esq ? Color.red : Color.green;
+        lr[0].endColor = bloq_esq ? Color.red : Color.green;
+        lr[1].startColor = bloq_esq ? Color.red : Color.green;
+        lr[1].endColor = bloq_esq ? Color.red : Color.green;
+
+        lr[2].startColor = bloq_cen ? Color.red : Color.green;
+        lr[2].endColor = bloq_cen ? Color.red : Color.green;
+        lr[3].startColor = bloq_cen ? Color.red : Color.green;
+        lr[3].endColor = bloq_cen ? Color.red : Color.green;
+
+        lr[4].startColor = bloq_dir ? Color.red : Color.green;
+        lr[4].endColor = bloq_dir ? Color.red : Color.green;
+        lr[5].startColor = bloq_dir ? Color.red : Color.green;
+        lr[5].endColor = bloq_dir ? Color.red : Color.green;
+
+        lr[0].SetPosition(0, new Vector3(17.5f, 0.5f, transform.position.z));
+        lr[0].SetPosition(1, new Vector3(17.5f, 0.5f, transform.position.z + 6f));
+
+        lr[1].SetPosition(0, new Vector3(17.5f, 2f, transform.position.z));
+        lr[1].SetPosition(1, new Vector3(17.5f, 0f, transform.position.z));
+
+        lr[2].SetPosition(0, new Vector3(20f, 0.5f, transform.position.z));
+        lr[2].SetPosition(1, new Vector3(20f, 0.5f, transform.position.z + 6f));
+
+        lr[3].SetPosition(0, new Vector3(20f, 2f, transform.position.z));
+        lr[3].SetPosition(1, new Vector3(20f, 0f, transform.position.z));
+
+        lr[4].SetPosition(0, new Vector3(22.5f, 0.5f, transform.position.z));
+        lr[4].SetPosition(1, new Vector3(22.5f, 0.5f, transform.position.z + 6f));
+
+        lr[5].SetPosition(0, new Vector3(22.5f, 2f, transform.position.z));
+        lr[5].SetPosition(1, new Vector3(22.5f, 0f, transform.position.z));
+
+        bool _carro = false;
+
         //proprio
         if (Physics.Raycast(transform.position + Vector3.forward, Vector3.forward, out RaycastHit _hit, 5f))
         {
@@ -70,7 +107,15 @@ public class Script_iaInfinityTudo : MonoBehaviour
                     Pulo();
                 }
             }
+
+            _carro = true;
         }
+
+        lr[6].startColor = _carro ? Color.red : Color.green;
+        lr[6].endColor = _carro ? Color.red : Color.green;
+
+        lr[6].SetPosition(0, transform.position + Vector3.forward);
+        lr[6].SetPosition(1, transform.position + Vector3.forward * 5);
 
         if (bloq_esq)
         {
