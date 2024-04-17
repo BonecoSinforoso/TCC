b72044efaced51608d2baf6a45a1f7fb31ccb9bd
Boneco Sinforoso <icmgc98@gmail.com>
2024-04-16 23:20:36 -0300

    flap mecanica de visualizacao

--- Assets/_ME/Scripts/Flap/Script_iaFlapTudo.cs
+++ Assets/_ME/Scripts/Flap/Script_iaFlapTudo.cs
@@ -9,13 +9,18 @@ public class Script_iaFlapTudo : MonoBehaviour
     [SerializeField] float alturaMin;
     [SerializeField] float coefY;
 
-    [SerializeField] GameObject[] obj_cano;
+    [SerializeField] Transform[] t_cano;
     [SerializeField] float distanciaCanoFrente;
     [SerializeField] float distanciaCanoAtras;
 
     [SerializeField] TextMeshProUGUI txt_pontos;
-    int pontos = 0;
 
+    [Header("Visualizacao")]
+    [SerializeField] LineRenderer lr_canoAlturaDif;
+    [SerializeField] LineRenderer lr_canoDistHor;
+    [SerializeField] LineRenderer lr_chao;
+
+    int pontos = 0;
     bool puloPode = true;
     bool movel;
 
@@ -29,34 +34,44 @@ public class Script_iaFlapTudo : MonoBehaviour
 
     void Update()
     {
+        int canoMaisProximoIndex = -1;
+
         if (movel)
         {
             //pra vc icaro do futuro
         }
         else
         {
-            for (int i = 0; i < obj_cano.Length; i++)
+            for (int i = 0; i < t_cano.Length; i++)
             {
-                if (transform.position.x < obj_cano[i].transform.position.x)
+                if (transform.position.x < t_cano[i].position.x) //atras do cano
                 {
-                    if (obj_cano[i].transform.position.x - transform.position.x <= distanciaCanoFrente)
+                    if (t_cano[i].transform.position.x - transform.position.x <= distanciaCanoFrente)
                     {
-                        if (transform.position.y + coefY < obj_cano[i].transform.position.y)
+                        if (transform.position.y + coefY < t_cano[i].position.y)
                         {
                             Pulo();
                         }
                     }
                 }
-                else
+                else //na frente do cano
                 {
-                    if (Mathf.Abs(transform.position.x - obj_cano[i].transform.position.x) < 1)
+                    if (Mathf.Abs(transform.position.x - t_cano[i].position.x) < 1)
                     {
-                        if (transform.position.y + coefY < obj_cano[i].transform.position.y)
+                        if (transform.position.y + coefY < t_cano[i].position.y)
                         {
                             Pulo();
                         }
                     }
                 }
+
+                //qual cano mais proximo
+                if (i != 0)
+                {
+                    if (Vector2.Distance(transform.position, t_cano[i].position) < Vector2.Distance(transform.position, t_cano[canoMaisProximoIndex].position))
+                        canoMaisProximoIndex = i;
+                }
+                else canoMaisProximoIndex = 0;
             }
         }
 
@@ -64,6 +79,15 @@ public class Script_iaFlapTudo : MonoBehaviour
         {
             Pulo();
         }
+
+        lr_canoAlturaDif.SetPosition(0, new Vector3(transform.position.x - 0.5f, transform.position.y, 0));
+        lr_canoAlturaDif.SetPosition(1, new Vector3(transform.position.x - 0.5f, t_cano[canoMaisProximoIndex].position.y, 0));
+
+        lr_canoDistHor.SetPosition(0, new Vector3(transform.position.x, transform.position.y + 0.5f, 0));
+        lr_canoDistHor.SetPosition(1, new Vector3(t_cano[canoMaisProximoIndex].position.x, transform.position.y + 0.5f, 0));
+
+        lr_chao.SetPosition(0, transform.position);
+        lr_chao.SetPosition(1, new Vector3(transform.position.x, -5, 0));
     }
 
     void Pulo()
